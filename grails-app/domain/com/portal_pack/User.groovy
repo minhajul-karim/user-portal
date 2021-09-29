a030790f288d77791c5d51100f2174601e9ad982
minhajul-karim <minhajul.kaarim@gmail.com>
2021-09-29 16:12:36 +0600

    Add necessary fields to User domain with constraints

--- grails-app/domain/com/portal_pack/User.groovy
+++ grails-app/domain/com/portal_pack/User.groovy
@@ -11,8 +11,13 @@ class User implements Serializable {
 
     private static final long serialVersionUID = 1
 
+    String firstName
+    String lastName
     String username
     String password
+    String phone
+    Date birthdate
+    String address
     boolean enabled = true
     boolean accountExpired
     boolean accountLocked
@@ -23,8 +28,13 @@ class User implements Serializable {
     }
 
     static constraints = {
-        password nullable: false, blank: false, password: true
-        username nullable: false, blank: false, unique: true
+        firstName size: 2..20, blank: false, 
+        lastName size: 2..20, blank: false, 
+        username blank: false, unique: true
+        password blank: false, password: true
+        phone size: 5..30, blank: false
+        birthdate blank: false
+        address size: 5..100, blank: false
     }
 
     static mapping = {
